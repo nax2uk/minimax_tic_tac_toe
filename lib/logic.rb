fbb97db32f600ddbd2eea69e3306a723f0ddf392
nax2uk <azlinayeo@gmail.com>
2020-09-26 15:53:48 +0100

    changed method spelling from entry to entries

--- lib/logic.rb
+++ lib/logic.rb
@@ -5,7 +5,7 @@ class Logic
     def check_win_horizontal(board)
         board.board_array.inject(false) {| win, row | win || self.same_entry_in_a_row?(row)}
     end
-    def same_entry_in_a_row?(board_row)
+    def same_entries_in_a_row?(board_row)
         board_row.uniq.size < 2  && board_row[0] != ""
     end
 end
--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,24 +2,24 @@ require './lib/logic'
 require './lib/board'
 
 describe Logic do
-    context "#same_entry_in_a_row?" do
+    context "#same_entries_in_a_row?" do
         logic = Logic.new
         board = Board.new
         it "checks if returns false if first row of board is all ''s" do
-            expect(logic.same_entry_in_a_row?(board.get_row("A"))).to eq(false)
+            expect(logic.same_entries_in_a_row?(board.get_row("A"))).to eq(false)
         end
         it "checks if returns true if first row of board is all 'X's" do
             board.set_row("A",["X","X","X"])
-            expect(logic.same_entry_in_a_row?(board.get_row("A"))).to eq(true)
+            expect(logic.same_entries_in_a_row?(board.get_row("A"))).to eq(true)
         end
         it "checks if returns false if second row of board is has two 'X's and one 'O's" do
             board.set_row("B",["X","","X"])
-            expect(logic.same_entry_in_a_row?(board.get_row("B"))).to eq(false)
+            expect(logic.same_entries_in_a_row?(board.get_row("B"))).to eq(false)
         end
     
         it "checks if returns false if third row of board is has one 'X's and one 'O's and one ''s " do
             board.set_row("C",["O","","X"])
-            expect(logic.same_entry_in_a_row?(board.get_row("C"))).to eq(false)
+            expect(logic.same_entries_in_a_row?(board.get_row("C"))).to eq(false)
         end
 
     end
