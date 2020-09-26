68dc979c07d5de5e520135da509f3eb947cb1dc0
nax2uk <azlinayeo@gmail.com>
2020-09-26 16:01:39 +0100

    added passed code for text check_win_horizontal

--- lib/logic.rb
+++ lib/logic.rb
@@ -2,8 +2,8 @@ class Logic
     def check_win(board)
 
     end
-    def check_win_horizontal(board)
-        board.board_array.inject(false) {| win, row | win || self.same_entry_in_a_row?(row)}
+    def check_win_horizontal?(board)
+        board.board_array.inject(false) {| win, row | win || self.same_entries_in_a_row?(row)}
     end
     def same_entries_in_a_row?(board_row)
         board_row.uniq.size < 2  && board_row[0] != ""
--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,9 +2,10 @@ require './lib/logic'
 require './lib/board'
 
 describe Logic do
+    let(:board) {Board.new}
+    let(:logic) {Logic.new}
     context "#same_entries_in_a_row?" do
-        logic = Logic.new
-        board = Board.new
+
         it "checks if returns false if first row of board is all ''s" do
             expect(logic.same_entries_in_a_row?(board.get_row("A"))).to eq(false)
         end
@@ -21,6 +22,22 @@ describe Logic do
             board.set_row("C",["O","","X"])
             expect(logic.same_entries_in_a_row?(board.get_row("C"))).to eq(false)
         end
-
+    end
+    context "#check_win_horizontal?" do
+        it "returns false if board has no entries" do
+            expect(logic.check_win_horizontal?(board)).to eql(false)
+        end
+        it "returns true if board has all 'X's in first row" do
+            board.set_row("A",["X","X","X"])
+            expect(logic.check_win_horizontal?(board)).to eql(true)
+        end
+        it "returns true if board has all 'X's in last row" do
+            board.set_row("C",["X","X","X"])
+            expect(logic.check_win_horizontal?(board)).to eql(true)
+        end
+        it "returns false if board has not all 'X's in last row" do
+            board.set_row("C",["X","","X"])
+            expect(logic.check_win_horizontal?(board)).to eql(false)
+        end
     end
 end
