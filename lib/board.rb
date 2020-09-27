2ff1490420536ec9620aea1f8402d043e2b8cf3c
nax2uk <azlinayeo@gmail.com>
2020-09-27 10:11:17 +0100

    added COL hash for column and refactored code accordingly

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,19 @@
 class Board
-    attr_accessor :board_array, :ROW
+    attr_accessor :board_array
 
     ROW = {
         'A'=>0,
         'B'=>1,
         'C'=>2 }.freeze
 
+    COL = {
+        '1' => 0,
+        '2' => 1,
+        '3' => 2
+}.freeze
+
     def initialize
         @board_array = Array.new(3) { Array.new(3,"") }
-        @ROW = ROW
     end
 
     def draw_board
@@ -27,18 +32,22 @@ class Board
 
     def set_entry(symbol, row, column)
         if symbol == 'X' || 'O' || ''
-            @board_array[ROW[row]][column.to_i - 1] = symbol
+            @board_array[ROW[row]][COL[column]] = symbol
         else
             raise ArgumentError.new("Input must 'X', 'O' or ''")
         end
     end
 
-    def get_row(row_letter)
-        @board_array[ROW[row_letter]]
+    def is_entry_on_a_left_diagonal?(row, column)
+        ROW[row] == 0 && COL[column] == 0 || ROW[row] == 1 && COL[column] == 1 || ROW[row] == 2 && COL[column] == 2
     end
 
-    def get_column(number)
-        index = number.to_i - 1
+    def get_row(row)
+        @board_array[ROW[row]]
+    end
+
+    def get_column(column)
+        index = COL[column]
         column = []
         column << @board_array[0][index] << @board_array[1][index] << @board_array[2][index]
     end
@@ -55,14 +64,14 @@ class Board
         end
     end
 
-    def set_row(row_letter, row_entries_array)
-        @board_array[ROW[row_letter]] = row_entries_array
+    def set_row(row, row_array)
+        @board_array[ROW[row]] = row_array
     end
 
-    def set_column(column_number, column_entries_array)
-        column_index = column_number.to_i - 1
-        for i in 0..2
-            @board_array[i][column_index] = column_entries_array[i]
+    def set_column(column, column_array)
+        #column_index = column_number.to_i - 1
+        for row_index in 0..2
+            @board_array[row_index][COL[column]] = column_array[row_index]
         end
     end
     
